a1057a3392b7f6cb9178b4bc308669ec8278ebc6
Shamil <mt89vein@gmail.com>
2017-08-19 18:22:01 +0500

    Mappings

---
+++ dirhash/Mappings/FileEntityMap.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using dirhash.Models;
+
+namespace dirhash.Mappings
+{
+    public class FileEntityMap : EntityTypeConfiguration<FileEntity>
+    {
+        public FileEntityMap()
+        {
+            ToTable("file");
+            HasKey(s => s.Id).Property(s => s.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+        }
+    }
+}
---
+++ dirhash/Mappings/LogEntityMap.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using dirhash.Models;
+
+namespace dirhash.Mappings
+{
+    public class LogEntityMap : EntityTypeConfiguration<LogEntity>
+    {
+        public LogEntityMap()
+        {
+            ToTable("log");
+            HasKey(s => s.Id).Property(s => s.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+        }
+    }
+}
