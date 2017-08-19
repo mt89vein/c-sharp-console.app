4df5559eaff0ae327dc44ec11df6c54f7be17f11
Shamil <mt89vein@gmail.com>
2017-08-19 18:21:46 +0500

    Models

---
+++ dirhash/Models/FileEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dirhash.Models
+{
+    /// <summary>
+    /// Файлы
+    /// </summary>
+    public class FileEntity : BaseEntity
+    {
+        /// <summary>
+        /// Название файла
+        /// </summary>
+        public string Filename { get; set; }
+        /// <summary>
+        /// Хэш сумма файла
+        /// </summary>
+        public string Hash { get; set; }
+        ///<summary>
+        /// Время создания записи
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+    }
+
+}
---
+++ dirhash/Models/LogEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dirhash.Models
+{
+    /// <summary>
+    /// Логирование
+    /// </summary>
+    public class LogEntity : BaseEntity
+    {
+        /// <summary>
+        /// Сообщение
+        /// </summary>
+        public string Message { get; set; }
+        /// <summary>
+        /// Время ошибки
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
